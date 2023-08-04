9abeae3471e4bf4e5cf76aac37536bf5c8426ac1
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-03 19:03:31 -0700

    DS - FIX: Removed unused imports

--- src/test/java/edu/ucsb/cs156/example/controllers/SystemInfoControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/SystemInfoControllerTests.java
@@ -8,7 +8,6 @@ import edu.ucsb.cs156.example.services.SystemInfoService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
