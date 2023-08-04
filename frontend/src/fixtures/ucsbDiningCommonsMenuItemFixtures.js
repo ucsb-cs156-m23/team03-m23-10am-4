72dd14a18c9073d1804196dedfc7301b04839e1b
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-04 14:44:23 -0700

    ADD: Fixtures for UCSBDCMI

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,33 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneItem : [
+        {
+            "id": 1,
+            "diningCommonsCode": "Carrillo",
+            "name": "Chicken",
+            "station": "Grill"
+        }
+    ],
+
+    threeItems : [
+        {
+            "id": 1,
+            "diningCommonsCode": "Carrillo",
+            "name": "Chicken",
+            "station": "Grill"
+        },
+        {
+            "id": 2,
+            "diningCommonsCode": "Carrillo",
+            "name": "Beef",
+            "station": "Grill"
+        },
+        {
+            "id": 3,
+            "diningCommonsCode": "Carrillo",
+            "name": "Pork",
+            "station": "Grill"
+        }
+    ]
+};
+
+export {ucsbDiningCommonsMenuItemFixtures};
