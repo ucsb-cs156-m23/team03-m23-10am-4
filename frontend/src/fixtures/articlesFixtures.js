5b21fc98c59efae977e054f30afddc7a4e7deee6
Matt Priston <mpristoncarruthersfi@ucsb.edu>
2023-08-04 16:00:54 -0700

    added fixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "Article 1",
+        "url": "https://www.article1.com",
+        "explanation": "This is article 1",
+        "email": "email1@gmail.com",
+        "dateAdded": "2023-03-26T12:00:00"
+    },
+    threeArticles: [
+        {
+            "id": 1,
+            "title": "Article 1",
+            "url": "https://www.article1.com",
+            "explanation": "This is article 1",
+            "email": "email1@gmail.com",
+            "dateAdded": "2023-03-26T12:00:00"
+
+        },
+        {
+            "id": 2,
+            "title": "Article 2",
+            "url": "https://www.article2.com",
+            "explanation": "This is article 2",
+            "email": "email2@gmail.com",
+            "dateAdded": "2023-03-26T12:00:00"
+        },
+        {
+            "id": 3,
+            "title": "Article 3",
+            "url": "https://www.article3.com",
+            "explanation": "This is article 3",
+            "email": "email3@gmail.com",
+            "dateAdded": "2023-03-26T12:00:00"
+        }
+    ]
+};
+
+export {articlesFixtures};
