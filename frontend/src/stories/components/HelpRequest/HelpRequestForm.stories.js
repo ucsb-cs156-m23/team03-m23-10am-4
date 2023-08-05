7cf9885836a7f28641b8eb8cbc3818c13901ed71
iain <cakefoxmc@gmail.com>
2023-08-05 04:02:28 +0000

    iw - fixed typo in HelpRequestForm update story

--- frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.js
+++ frontend/src/stories/components/HelpRequest/HelpRequestForm.stories.js
@@ -27,7 +27,7 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-    initialContents: helpRequestFixtures.oneDate,
+    initialContents: helpRequestFixtures.oneHelpRequest,
     buttonLabel: "Update",
     submitAction: (data) => {
         console.log("Submit was clicked with data: ", data); 
