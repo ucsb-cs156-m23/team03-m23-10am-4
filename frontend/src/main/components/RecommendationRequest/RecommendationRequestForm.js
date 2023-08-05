da534cd5bb6de23411da76ac8150991bb0e74d8d
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-05 02:40:38 -0700

    FIX: sotrybook now complete

--- frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
@@ -3,16 +3,6 @@ import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom';
 
 function RecommendationRequestForm({ initialContents, submitAction, buttonLabel = "Create" }) {
-
-    /*
-    private String requesterEmail;
-    private String professorEmail;
-    private String explanation;
-    private LocalDateTime dateRequested;
-    private LocalDateTime dateNeeded;
-    private boolean done;
-    
-    */
     
     // Stryker disable all
     const {
@@ -46,13 +36,13 @@ function RecommendationRequestForm({ initialContents, submitAction, buttonLabel 
             )}
 
             <Form.Group className="mb-3" >
-                <Form.Label htmlFor="email">Email</Form.Label>
+                <Form.Label htmlFor="requesterEmail">Email</Form.Label>
                 <Form.Control
                     data-testid={testIdPrefix + "-requesterEmail"}
-                    id="email"
+                    id="requesterEmail"
                     type="text"
                     isInvalid={Boolean(errors.name)}
-                    {...register("email", {
+                    {...register("requesterEmail", {
                         required: "Your email is required.",
                         maxLength : {
                             value: 100,
@@ -61,7 +51,7 @@ function RecommendationRequestForm({ initialContents, submitAction, buttonLabel 
                     })}
                 />
                 <Form.Control.Feedback type="invalid">
-                    {errors.email?.message}
+                    {errors.requesterEmail?.message}
                 </Form.Control.Feedback>
             </Form.Group>
 
@@ -137,19 +127,19 @@ function RecommendationRequestForm({ initialContents, submitAction, buttonLabel 
                 </Form.Control.Feedback>
             </Form.Group>         
 
-            <Form.Group className="mb-3" >
+            {/* <Form.Group className="mb-3" >
                 <Form.Label htmlFor="done">Done</Form.Label>
                 <Form.Control
                     data-testid={testIdPrefix + "-done"}
                     id="done"
-                    type="checkbox"
+                    type="radio"
                     isInvalid={Boolean(errors.done)}
                     {...register("done")}
                 />
                 <Form.Control.Feedback type="invalid">
                     {errors.done?.message}
                 </Form.Control.Feedback>
-            </Form.Group>
+            </Form.Group> */}
 
 
             <Button
--- frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemRequestForm.stories.js
+++ frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemRequestForm.stories.js
@@ -26,7 +26,7 @@ Create.args = {
 export const Update = Template.bind({});
 
 Update.args = {
-    initialContents: ucsbDiningCommonsMenuItemFixtures.oneItem,
+    initialContents: ucsbDiningCommonsMenuItemFixtures.oneItem[0],
     buttonLabel: "Update",
     submitAction: (data) => {
         console.log("Submit was clicked with data: ", data); 
