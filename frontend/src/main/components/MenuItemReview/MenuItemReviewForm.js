d4f12aeba535f0575ad6bf50d86abe5d46ce7a5d
publicqi <qisu@ucsb.edu>
2023-08-06 15:51:50 -0700

    qs - fix stryker mutation for regex

--- frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -19,8 +19,11 @@ function MenuItemReviewForm({ initialContents, submitAction, buttonLabel = "Crea
 
     // Stryker disable next-line Regex
     const int_regex = /\d+/;
+    // Stryker disable next-line Regex
     const email_regex = /\S+@\S+\.\S+/;
+    // Stryker disable next-line Regex
     const stars_regex = /[1|2|3|4|5]/;
+    // Stryker disable next-line Regex
     const isodate_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
 
 
