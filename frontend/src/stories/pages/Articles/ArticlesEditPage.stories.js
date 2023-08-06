c9781d324c08fdf5b5120ef7d634231d29104d56
Matt Priston <mpristoncarruthersfi@ucsb.edu>
2023-08-06 10:47:56 -0700

    added storybook

--- frontend/src/stories/pages/Articles/ArticlesEditPage.stories.js
+++ frontend/src/stories/pages/Articles/ArticlesEditPage.stories.js
@@ -2,17 +2,17 @@
 import React from 'react';
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-import { ucsbDatesFixtures } from "fixtures/ucsbDatesFixtures";
+import { articlesFixtures } from "fixtures/articlesFixtures";
 import { rest } from "msw";
 
-import UCSBDatesEditPage from "main/pages/UCSBDates/UCSBDatesEditPage";
+import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
 
 export default {
-    title: 'pages/UCSBDates/UCSBDatesEditPage',
-    component: UCSBDatesEditPage
+    title: 'pages/Articles/ArticlesEditPage',
+    component: ArticlesEditPage
 };
 
-const Template = () => <UCSBDatesEditPage storybook={true}/>;
+const Template = () => <ArticlesEditPage storybook={true}/>;
 
 export const Default = Template.bind({});
 Default.parameters = {
@@ -23,10 +23,10 @@ Default.parameters = {
         rest.get('/api/systemInfo', (_req, res, ctx) => {
             return res(ctx.json(systemInfoFixtures.showingNeither));
         }),
-        rest.get('/api/ucsbdates', (_req, res, ctx) => {
-            return res(ctx.json(ucsbDatesFixtures.threeDates[0]));
+        rest.get('/api/articles', (_req, res, ctx) => {
+            return res(ctx.json(articlesFixtures.threeArticles[0]));
         }),
-        rest.put('/api/ucsbdates', async (req, res, ctx) => {
+        rest.put('/api/articles', async (req, res, ctx) => {
             var reqBody = await req.text();
             window.alert("PUT: " + req.url + " and body: " + reqBody);
             return res(ctx.status(200),ctx.json({}));
