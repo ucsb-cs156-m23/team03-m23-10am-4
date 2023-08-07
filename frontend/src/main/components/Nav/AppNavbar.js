c785b798afd383f29abf9d228d27d72fffb0b7f6
Yuanchen Meng <mych12345@126.com>
2023-08-07 13:25:00 -0700

    ym - created Placeholder pages

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -62,6 +62,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   <Nav.Link as={Link} to="/placeholder">Placeholder</Nav.Link>
                   <Nav.Link as={Link} to="/menuitemreview">Menu Item Review</Nav.Link>
                   <Nav.Link as={Link} to="/articles">Articles</Nav.Link>
+                  <Nav.Link as={Link} to="/ucsborganization">UCSB Organizations</Nav.Link>
                 </>
               )
             }
