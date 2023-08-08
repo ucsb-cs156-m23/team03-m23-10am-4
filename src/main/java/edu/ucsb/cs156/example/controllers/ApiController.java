db2cec40f468c373a85755b58e100f6c4bb39bd0
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-07 19:22:57 -0700

    fix:unused imports part 2

--- src/main/java/edu/ucsb/cs156/example/controllers/ApiController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ApiController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.ucsb.cs156.example.models.CurrentUser;
 import edu.ucsb.cs156.example.services.CurrentUserService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
