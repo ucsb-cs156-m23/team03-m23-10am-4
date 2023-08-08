d15db81a13805c94e39cb6a46db4ca410c376d64
gento <41165204+dscpsyl@users.noreply.github.com>
2023-08-07 19:17:38 -0700

    feat:create page

--- frontend/src/main/pages/RecomendationRequest/RecomendationRequestCreatePage.js
+++ frontend/src/main/pages/RecomendationRequest/RecomendationRequestCreatePage.js
@@ -1,12 +1,51 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function RecomendationRequestCreatePage() {
+export default function RestaurantCreatePage({storybook=false}) {
 
-  // Stryker disable all : placeholder for future implementation
+  const objectToAxiosParams = (request) => ({
+    url: "/api/recommendationrequest/post",
+    method: "POST",
+    params: {
+      id: request.id,
+      requesterEmail: request.requesterEmail,
+      professorEmail: request.professorEmail,
+      explanation: request.explanation,
+      dateRequested: request.dateRequested,
+      dateNeeded: request.dateNeeded,
+      done: request.done,
+    }
+  });
+
+  const onSuccess = (request) => {
+    toast(`New request Created - id: ${request.id}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+     { onSuccess }, 
+     // Stryker disable next-line all : hard to set up test for caching
+     ["/api/recommendationrequest/all"] // mutation makes this key stale so that pages relying on it reload
+     );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/recommendationrequest" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Create page not yet implemented</h1>
+        <h1>Create New Request</h1>
+        <RecommendationRequestForm submitAction={onSubmit} />
       </div>
     </BasicLayout>
   )
