8710c08081c54de0fe66f4d2aeac673e146ac536
publicqi <qisu@ucsb.edu>
2023-08-08 19:28:09 -0700

    fix: change code for test

--- frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
+++ frontend/src/main/components/MenuItemReview/MenuItemReviewForm.js
@@ -122,15 +122,12 @@ function MenuItemReviewForm({ initialContents, submitAction, buttonLabel = "Crea
                     type="datetime-local"
                     isInvalid={Boolean(errors.dateReviewed)}
                     {...register("dateReviewed", {
-                        required: "Date Reviewed is required.",
-                        pattern: {
-                            value: isodate_regex,
-                            message: "Date Reviewed must be a valid date"
-                        },
+                        required: true,
+                        pattern: isodate_regex,
                     })}
                 />
                 <Form.Control.Feedback type="invalid">
-                    {errors.dateReviewed?.message}
+                    {errors.dateReviewed && "Date Reviewed is required."}
                 </Form.Control.Feedback>
             </Form.Group>
 
--- frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.js
+++ frontend/src/tests/components/MenuItemReview/MenuItemReviewForm.test.js
@@ -84,7 +84,6 @@ describe("MenuItemReviewForm tests", () => {
         expect(screen.queryByText(/Item Id must be a number/)).not.toBeInTheDocument();
         expect(screen.queryByText(/Reviewer Email must be a valid email address/)).not.toBeInTheDocument();
         expect(screen.queryByText(/Stars must be a number between 1 and 5/)).not.toBeInTheDocument();
-        expect(screen.queryByText(/Date Reviewed must be a valid date/)).not.toBeInTheDocument();
     });
 
     test("that navigate(-1) is called when Cancel is clicked", async () => {
@@ -128,6 +127,6 @@ describe("MenuItemReviewForm tests", () => {
         await screen.findByText(/Item Id must be a number/);
         expect(screen.getByText(/Reviewer Email must be a valid email address/)).toBeInTheDocument();
         expect(screen.getByText(/Stars must be a number between 1 and 5/)).toBeInTheDocument();
-        // expect(screen.getByText(/Date Reviewed must be a valid date/)).toBeInTheDocument();
+        expect(screen.getByText(/Date Reviewed is required/)).toBeInTheDocument();
     });
 });
