29a291f9f66d50d0d8e46638e6e06199f085a983
iain <cakefoxmc@gmail.com>
2023-08-10 14:01:30 -0700

    iw - fixed menuitem edit page and remove placeholder from navbar

--- frontend/src/stories/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemEditPage.stories.js
+++ frontend/src/stories/pages/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemEditPage.stories.js
@@ -23,10 +23,10 @@ Default.parameters = {
         rest.get('/api/systemInfo', (_req, res, ctx) => {
             return res(ctx.json(systemInfoFixtures.showingNeither));
         }),
-        rest.get('/api/helprequest', (_req, res, ctx) => {
+        rest.get('/api/ucsbdiningcommonsmenuitem', (_req, res, ctx) => {
             return res(ctx.json(ucsbDiningCommonsMenuItemFixtures.threeItems[0]));
         }),
-        rest.put('/api/helprequest', async (req, res, ctx) => {
+        rest.put('/api/ucsbdiningcommonsmenuitem', async (req, res, ctx) => {
             var reqBody = await req.text();
             window.alert("PUT: " + req.url + " and body: " + reqBody);
             return res(ctx.status(200),ctx.json({}));
